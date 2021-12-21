15ca9990f4737365a637d90e0df801bd8b2d920e
Hesahm Atef Mohamed <90654024+HeshamAtefMohamed@users.noreply.github.com>
2021-12-21 20:50:22 +0200

    Add files via upload

--- WeatherJouranlApp/website/app.js
+++ WeatherJouranlApp/website/app.js
@@ -1,73 +1,67 @@
 /* Global Variables */
-// set a variable for api key  
+//setup a var for apiKey, that gotten from OpenWeatherMap.org
 const apiKey="b0bacaabb7fdad1e4acc8f5acc90f4c7";
-// Create a new date instance dynamically with JS     
+//setup a var for api call link
+const apiUrl ='http://api.openweathermap.org/data/2.5/weather?zip=';
+
+// Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-//Second APi&Routes
-    //Start with an event on the generate button that when click on button, after entering data a specific fun will be excuted
+//get the generate button
+const generate=document.querySelector("#generate");
+//add event listner on butten, that when click a callback fun will be excuted
+generate.addEventListener("click",generateFun)
 
-        //call the button
-        const generateButton=document.querySelector("#generate");
-        // set an event from an  event listener click& async fun on the button 
-        generateButton.addEventListener("click",async()=>{
-        
-        /*to ensure every thing works clearly, use try and catch
-        If ok then try will running, else then catch will run*/  
-        try{
-            // set a variable for getWeatherData fun
-                const temp=  getWeatherData(zipCode);        
-        //use fetch request to send requests to server side with sytax"https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch"
-            //As /saveData endpoint & post request
-            fetch('/saveData', {
-                method: 'POST', // or 'PUT'
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    // the  three child divs with the ids
-                        date:newDate,
-                        temp:temp,
-                        feelings:feelings
-                })
-            });
-        //use fetch request to send requests to server side
-        //As /getData endpoint & get request
-        fetch('/getData', {
-            method: 'get', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                // the  three child divs with the ids
-                    date:newDate,
-                    temp:temp,
-                    feelings:feelings
-            })
+//Built the callback fun of the eventListener
+function generateFun(){
+    //user entered zip code
+    const zipCode =document.querySelector("#zip").value;
+    //user entered feelings
+    const feelings=document.querySelector("#feelings").value;
+    //call for call  async GET request with the there parameters
+    getWeatherData(apiUrl,zipCode,apiKey).then(function (data) {
+            //setup a post request, which POST route is setup to add these values(temp ,date ,feelings)
+            postWeatherData("/saveData", { temp: data.main.temp, date: newDate, feelings: feelings });
         });
-                }catch(error){
-                console.log(error);
-                };     
-        
-        });
-
-async function getWeatherData(zipCode){
-    // set a variable for call link of api with zipCode & apiKey with units following metric    
-        const apiLink=`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`
-    //get the zipCode value   
-        const zipCode=document.querySelector("#zip").value;
-    //get the felling 
-        const feelings=document.querySelector("#feelings");
-    //the main usage for this fun is when click the button, entered data is collected
-        // use fetch method to get data
-            const res= await fetch(apiLink)
-        //transform the the collected data form stream form to JS object form by json"pasrsing"
-            //await is for an async operation 
-                const resData=await res.json();
-            // make a variable to get the required info from the coming resData(temperature)
-                const temp=resData.main.temp;
-            //check step, data is collected successfully
-                console.log("Data is successfully returned from the external API.");
-        
+        console.log(data);
 };
+
+//declaration of getWeatherData (get request function)
+const getWeatherData=async (apiUrl, zipCode, apiKey)=>{
+
+    const res=await fetch(apiUrl+ zipCode+"&appid=" + apiKey+"&units=metric");
+    //for resolved value, solve the fun
+    try{
+        const data= await res.json();
+        console.log(data);
+        return data
+    }
+    //for not resolved value, get the error
+    catch(error){
+        console.log("error",error);
+    }
+};
+
+//Declare the postWeatherData var for fun 
+const postWeatherData= async(url = '', data = {})=>{
+     //use fetch request to send requests to server side with sytax"https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch"
+       
+     const res= await fetch('/saveData', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    //Do the resolved code
+    try{
+        const data= await res.json();
+        return data;
+    }
+    //show the error for not resolved code
+    catch(error){
+        console.log("error",error)
+    }
+    console.log(data); 
+}
